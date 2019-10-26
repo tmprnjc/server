ee287f33302f1197fcce3c59949725c2e4b79e2f
tomiella <tomiella@icloud.com>
2019-10-26 23:24:40 +0200

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-app.listen(process.env.PORT,  () => console.log('Starting server: {process.env.PORT}'));
+app.listen(3000,  () => console.log('Starting server: {process.env.PORT}'));
 app.use(express.static('public'));
 
 app.post('/velox', (req, res) => {
